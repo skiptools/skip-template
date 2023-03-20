9274622dde0ed7e726a91fa4e1b20bbb0d8248ad
Marc Prud'hommeaux <marc@prux.org>
2023-03-20 18:54:24 -0400

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -4,13 +4,7 @@ import PackageDescription
 let package = Package(
     name: "skip-template",
     defaultLocalization: "en",
-    platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8),
-        .macCatalyst(.v15),
-    ],
+    platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v8), .macCatalyst(.v15)],
     products: [
         .library(name: "DemoLib", targets: ["DemoLib"]),
         .library(name: "DemoLibKotlin", targets: ["DemoLibKotlin"]),
